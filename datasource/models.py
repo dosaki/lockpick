d0aa902d13d99bdb8f0129772b9cb6262c4fda3f
Tiago Correia <lord.g.bug@gmail.com>
2016-09-30 23:24:33 +0100

    Adding entry points.

--- datasource/models.py
+++ datasource/models.py
@@ -12,11 +12,53 @@ class User(Base, I_Serializable):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), unique=True)
-    secret_key = Column(String(64))
+    secret = Column(String(64))
 
     def __init__(self, name=None):
         self.name = name
-        self.secret_key = hexlify(os.urandom(64))
+        self.secret = hexlify(os.urandom(64))
 
     def __repr__(self):
         return '<User %r>' % (self.name)
+
+class Game(Base, I_Serializable):
+    __tablename__ = 'games'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), unique=True)
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def __repr__(self):
+        return '<Game %r>' % (self.name)
+
+class Lock(Base, I_Serializable):
+    __tablename__ = 'locks'
+    id = Column(Integer, primary_key=True)
+    key = Column(String(256), unique=False)
+    clue = Column(String(256), unique=False)
+    treasure = Column(String(256), unique=False)
+    game = Column('game_id', Integer, ForeignKey("game.id"), nullable=False)
+
+    def __init__(self, name=None, key=None, clue=None, treasure=None):
+        self.name = name
+        self.key = key
+        self.clue = clue
+        self.treasure = treasure
+
+    def __repr__(self):
+        return '<Lock %r: %r, %r, %r>' % (self.name, self.key, self.clue, self.treasure)
+
+class UserProgress(Base, I_Serializable):
+    __tablename__ = 'users_progress'
+    id = Column(Integer, primary_key=True)
+    user = Column('user_id', Integer, ForeignKey("user.id"), nullable=False)
+    lock = Column('lock_id', Integer, ForeignKey("game.id"), nullable=False)
+
+    def __init__(self, user=None, lock=None):
+        self.name = name
+        self.lock = lock
+
+    def __repr__(self):
+        percentage = (self.lock.order / self.lock.game.total) * 100
+        return '<UserProgress %r (%r): %r>' % (self.user.name, self.lock.game.name, percentage)
--- index.py
+++ index.py
@@ -13,10 +13,36 @@ def unlock(key):
 
 @app.route('/register/<username>')
 def register(username):
-    u = User(username)
-    db_session.add(u)
+    user = User(username)
+    db_session.add(user)
     db_session.commit()
-    return app.jsonify(**(u.as_dict()))
+    return app.jsonify(**(user.as_dict()))
+
+@app.route('/view/<username>')
+def view(username):
+    user = User.query.filter(User.name == username).first()
+    return app.jsonify(name=user.name)
+
+@app.route('/progress/<game>/<username>')
+def progress(game, username):
+    user_progress = UserProgress.query.filter(and_(User.name == username, Game.name == game)).first()
+    return app.jsonify(name=user.name, game=game.as_dict())
+
+@app.route('/new/<game>')
+def newGame(game):
+    game = Game(game)
+    db_session.add(game)
+    db_session.commit()
+    return app.jsonify(**(game.as_dict()))
+
+@app.route('/addLock/<game>')
+def addLock(game):
+    secret = request.args.get('secret')
+    username = request.args.get('username')
+    
+    user = User.query.filter(and_(User.secret == secret, User.name == username)).first()
+    game = Game.query.filter(Game.name == game).first()
+    return app.jsonify(**(game.as_dict()))
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
